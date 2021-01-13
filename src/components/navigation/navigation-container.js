34e8df4da8b9a0bfd60fcd370821dfa07a78408e
Mr. Decker <decker389@aol.com>
2021-01-12 23:29:52 -0700

    added dynamic links to portfolio items for details and finished adding links to navigation bar

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -15,9 +15,8 @@ export default class NavigationComponent extends Component {
                 </NavLink>
 
                 <NavLink to="/about-me">About</NavLink>
-                
-                <button>Contact</button>
-                <button>Blog</button>
+                <NavLink to="/contact">Contact</NavLink>
+                <NavLink to="/blog">Blog</NavLink>
                 {false ? <button>Add Blog</button>: null }
             </div>
         );
