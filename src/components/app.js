f478644a142ed5c32092f142b52f33c3c5891403
Mr. Decker <decker389@aol.com>
2021-01-13 18:24:59 -0700

    attempted fix to portfolio slug exact Route not working correctly, didn't work

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import moment from "moment";
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import NavigationContainer from "./navigation/navigation-Container";
+import NavigationContainer from "./navigation/navigation-container";
 import Home from "./pages/home";
 import About from "./pages/about";
 import Contact from "./pages/contact";
@@ -13,22 +13,21 @@ import NoMatch from "./pages/no-match";
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-
+      <div className="app">
         <Router>
           <div>
-          <h1>The Portfolio</h1>
-        <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
-          <NavigationContainer />
+            <h1>Jordan Hudgens Portfolio</h1>
+            <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
+            <NavigationContainer />
 
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/about-me" component={About} />
-            <Route path="/contact" component={Contact} />
-            <Route path="/blog" component={Blog} />
-            <Route exact path="/portfolio/:slug" component={PortfolioDetail} />
-            <Route component={NoMatch} />
-          </Switch>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about-me" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/blog" component={Blog} />
+              <Route exact path="/portfolio/:slug" component={PortfolioDetail} />
+              <Route component={NoMatch} />
+            </Switch>
           </div>
         </Router>
       </div>
--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,24 +1,21 @@
 import React, { Component } from "react";
-import { NavLink } from 'react-router-dom';
-
+import { NavLink } from "react-router-dom";
 
 export default class NavigationComponent extends Component {
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+  }
 
-    render() {
-        return (
-            <div>
-                <NavLink exact to="/" >
-                    Home
-                </NavLink>
+  render() {
+    return (
+      <div>
+        <NavLink exact to="/" activeClassName="nav-link-active">Home</NavLink>
+        <NavLink to="/about-me" activeClassName="nav-link-active">About</NavLink>
+        <NavLink to="/contact" activeClassName="nav-link-active">Contact</NavLink>
+        <NavLink to="/blog" activeClassName="nav-link-active">Blog</NavLink>
 
-                <NavLink to="/about-me">About</NavLink>
-                <NavLink to="/contact">Contact</NavLink>
-                <NavLink to="/blog">Blog</NavLink>
-                {false ? <button>Add Blog</button>: null }
-            </div>
-        );
-    }
+        {false ? <button>Add Blog</button> : null}
+      </div>
+    );
+  }
 }
