804006627f6b647c06c30a0f61a07ffae48c7d1b
Mr. Decker <decker389@aol.com>
2021-01-20 13:34:51 -0700

    changed auth components so that child component passes authorization to a parent class so that we can have a state keep the loggedIn status true when we refresh the page. changed navigation-container from a class component to a functional component.

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import axios from "axios";
 
 import NavigationContainer from "./navigation/navigation-container";
 import Home from "./pages/home";
@@ -11,7 +12,60 @@ import Auth from "./pages/auth";
 import NoMatch from "./pages/no-match";
 
 export default class App extends Component {
- 
+ constructor(props) {
+   super(props);
+
+   this.state = {
+     loggedInStatus: "NOT_LOGGED_IN"
+   };
+
+   this.handleSuccessfulLogin = this.handleSuccessfulLogin.bind(this);
+   this.handleUnSuccessfulLogin = this.handleUnsuccessfulLogin.bind(this);
+ }
+
+ handleSuccessfulLogin() {
+   this.setState({
+     loggedInStatus: "LOGGED_IN"
+   })
+ }
+
+ handleUnsuccessfulLogin() {
+   this.setState({
+     loggedInStatus: "NOT_LOGGED_IN"
+   })
+ }
+
+ checkLoginStatus() {
+    return axios.get("https://api.devcamp.space/logged_in", {withCredentials: true
+  }).then(response => {
+    const loggedIn = response.data.logged_in;
+    const loggedInStatus = this.state.loggedInStatus;
+
+    // If loggedIn and status LOGGED_IN => return data
+    // If loggedIN status NOT_LOGGED_IN => update state
+    // IF not loggedIn and status LOGGED_IN => update state
+
+    if (loggedIn && loggedInStatus === "LOGGED_IN") {
+      return loggedIn;
+    } else if (loggedIn && loggedInStatus === "NOT_LOGGED_IN") {
+      this.setState({
+        loggedInStatus: "LOGGED_IN"
+      });
+    } else if (!loggedIn && loggedInStatus === "LOGGED_IN") {
+      this.setState({
+        loggedInStatus: "NOT_LOGGED_IN"
+      });
+    }
+  })
+  .catch(error => {
+    console.log("Error", error)
+  })
+ }
+
+ componentDidMount() {
+   this.checkLoginStatus();
+ }
+
   render() {
     return (
       <div className="container">
@@ -19,9 +73,22 @@ export default class App extends Component {
           <div>
             <NavigationContainer />
 
+            <h2>{this.state.loggedInStatus}</h2>
+
             <Switch>
               <Route exact path="/" component={Home} />
-              <Route path="/auth" component={Auth} />
+
+              <Route 
+              path="/auth"
+              render={props => (
+                  <Auth 
+                  {...props}
+                  handleSuccessfulLogin={this.handleSuccessfulLogin}
+                  handleUnsuccessfulLogin={this.handleUnSuccessfulLogin}
+                  />
+              )}
+              />
+
               <Route path="/about-me" component={About} />
               <Route path="/contact" component={Contact} />
               <Route path="/blog" component={Blog} />
