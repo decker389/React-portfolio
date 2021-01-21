fe06d8de0202e1c1e78d1347c4824990acae0286
Mr. Decker <decker389@aol.com>
2021-01-21 16:04:03 -0700

    added portfolio manager styles

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -3,8 +3,14 @@ import React, { Component } from 'react';
 export default class PortfolioManager extends Component {
     render() {
         return (
-            <div>
-                <h1>PortfolioManager</h1>
+            <div className="portfolio-manager-wrapper">
+                <div className="left-column">
+
+                </div>
+
+                <div className="right-column">
+                    left Side
+                </div>
             </div>
         );
     }
