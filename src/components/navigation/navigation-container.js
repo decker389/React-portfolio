8e745b585399d7e1b38e9e0ea876ace1f8e00544
Mr. Decker <decker389@aol.com>
2021-01-20 23:41:31 -0700

    added in portfolio component and link that is only active when signed in

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -6,8 +6,10 @@ import { NavLink } from "react-router-dom";
 const NavigationComponent = (props) => {
   const dynamicLink = (route, linkText) => {
     return (
-      <div className="nav-link-wrapper">
-      <NavLink to="/blog" activeClassName="nav-link-active">Blog</NavLink>
+      <div>
+        <NavLink to={route} activeClassName="nav-link-active">
+         {linkText}
+        </NavLink>
       </div>
     );
   };
@@ -36,8 +38,10 @@ const NavigationComponent = (props) => {
           <div className="nav-link-wrapper">
             <NavLink to="/contact" activeClassName="nav-link-active">Contact</NavLink>
           </div>
-
-          {props.loggedInStatus === "LOGGED_IN" ? dynamicLink("/blog","Blog") : null}
+          <div className="nav-link-wrapper">
+            <NavLink to="/blog" activeClassName="nav-link-active">Blog</NavLink>
+          </div>
+          {props.loggedInStatus === "LOGGED_IN" ? dynamicLink("/portfolio-manager","Portfolio Manager") : null}
         </div>
         <div className="right-side">
           Chris Decker
