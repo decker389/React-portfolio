e7957f467497eebd11cfdb3cf5d123d7b018b760
Mr. Decker <decker389@aol.com>
2021-01-22 00:28:52 -0700

    added portfolio form file and created basics for form.

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 import PortfolioSidebarList from "../portfolio/portfolio-sidebar-list";
+import PortfolioForm from '../portfolio/portfolio-form';
 
 export default class PortfolioManager extends Component {
     constructor() {
@@ -9,7 +10,20 @@ export default class PortfolioManager extends Component {
 
         this.state = {
             portfolioItems: []
-        }
+        };
+
+        this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(this);
+        this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
+    }
+
+    handleSuccessfulFormSubmission(portfolioItem) {
+        //ToDO
+        //update the portfolioItems state
+        // and add the portfolioItem to the list
+    }
+
+    handleFormSubmissionError(error) {
+        console.log("handleFormSubmission error", error);
     }
 
     getPortfolioItems() {
@@ -35,7 +49,10 @@ export default class PortfolioManager extends Component {
             <div className="portfolio-manager-wrapper">
                 <div className="left-column">
                     <h1>
-                    left side
+                    <PortfolioForm 
+                        handleSuccessfulFormSubmission={this.handleSuccessfulFormSubmission}
+                        handleFormSubmissionError={this.handleFormSubmissionError}
+                    />
                     </h1>
                 </div>
 
