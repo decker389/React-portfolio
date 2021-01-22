94cef4bb50862acb5df181dc3fc1f5be6f6e50c1
Mr. Decker <decker389@aol.com>
2021-01-22 01:04:27 -0700

    continued adding protfolio manager form, fixed warnings on lists requiring unique keys.

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -16,18 +16,38 @@ export default class PortfolioForm extends Component {
         };
 
         this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    buildForm() {
+        let formData = new FormData()
+
+        formData.append("portfolio_item[name]", this.state.name);
+        formData.append("portfolio_item[description]", this.state.description);
+        formData.append("portfolio_item[url]", this.state.url);
+        formData.append("portfolio_item[catergory]", this.state.category);
+        formData.append("portfolio_item[position]", this.state.position);
+
+        return formData;
     }
 
     handleChange(event){
-        console.log("handle event", event);
+        this.setState({
+            [event.target.name]: event.target.value
+        })
     }
-    
+
+    handleSubmit(event) {
+        this.buildForm();
+        event.preventDefault();
+    }
+
     render() {
         return (
             <div>
                 <h1>Portfolio Form</h1>
 
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <div>
                         <input 
                         type="text"
