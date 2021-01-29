8c05c3217165d3dcae4456dcca2779df1790820b
Mr. Decker <decker389@aol.com>
2021-01-29 14:18:25 -0700

    started adding rich text editor functionality, will post new blog to blog list but is not parsing html into regular text yet.

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -1,17 +1,25 @@
 import React, { Component } from "react";
 import axios from "axios";
 
+import RichTextEditor from "../forms/rich-text-editor";
+
 export default class BlogForm extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
             title: "",
-            blog_status: ""
+            blog_status: "",
+            content: ""
         };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleRichTextEditorChange = this.handleRichTextEditorChange.bind(this);
+    }
+
+    handleRichTextEditorChange(content) {
+        this.setState({ content });
     }
 
     buildForm() {
@@ -19,19 +27,19 @@ export default class BlogForm extends Component {
 
         formData.append("portfolio_blog[title]", this.state.title);
         formData.append("portfolio_blog[blog_status", this.state.blog_status);
-
+        formData.append("portfolio_blog[content]", this.state.content);
         return formData;
     }
 
     handleSubmit(event) {
         axios.post("https://chrisdecker.devcamp.space/portfolio/portfolio_blogs", this.buildForm(), { withCredentials: true })
         .then(response => {
-            this.props.handleSuccessfullFormSubmission(response.data.portfolio_blog);
-
             this.setState({
                 title: "",
                 blog_status: ""
             });
+
+            this.props.handleSuccessfullFormSubmission(response.data.portfolio_blog);
         })
         .catch(error => {
             console.log("handleSubmit for blog error", error);
@@ -53,6 +61,11 @@ export default class BlogForm extends Component {
                 <div className="two-column">
                     <input type="text" onChange={this.handleChange} name="title" placeholder="Blog Title" value={this.state.title} />
                     <input type="text" onChange={this.handleChange} name="blog_status" placeholder="Blog Status" value={this.state.blog_status} />
+                </div>
+
+                <div className="one-column">
+                    <RichTextEditor handleRichTextEditorChange={this.handleRichTextEditorChange}
+                    />
                 </div>
 
                 <button className="btn">Save</button>
