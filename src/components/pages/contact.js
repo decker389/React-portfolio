361f63ee7cbe8e9e0cb132912d513f9dfe8d8910
Mr. Decker <decker389@aol.com>
2021-02-01 23:51:40 -0700

    updated contact page to include LinkedIn Profile and Github Profile. added Icons and Styles to that page. added temporary picture to contact page

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,7 +1,61 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import contactImage from "../../../static/assets/images/contact/contact.jpg";
 
 export default function() {
     return (
-        <div>Contact</div>
+        <div className="content-page-wrapper">
+            <div className="left-column" 
+            style={{
+                background: "url(" + contactImage + ") no-repeat",
+                backgroundSize: "cover",
+                backgroundPosition: "center"
+            }}
+            />
+
+            <div className="right-column">
+                <div className="contact-bullet-points">
+                    <div className="icon-wrapper">
+                        <div className="contact-icon">
+                            <FontAwesomeIcon icon="mobile" />
+                        </div>
+
+                        <div className="text">
+                            <p> Placeholder Phone Number</p>
+                        </div>
+                    </div>
+
+                    <div className="icon-wrapper">
+                        <div className="contact-icon">
+                            <FontAwesomeIcon icon="file" />
+                        </div>
+                        <div className="text">
+                            <p>Decker389@aol.com</p>
+                        </div>
+                    </div>
+
+                    <div className="icon-wrapper">
+                        <div className="contact-icon">
+                            <FontAwesomeIcon icon="paw" />
+                        </div>
+                        <div className="text">
+                            <a href="https://github.com/decker389">Github</a>    
+                        </div>
+                        
+                    </div>
+            
+                    <div className="icon-wrapper">
+                        <div className="contact-icon">
+                            <FontAwesomeIcon icon="link" />
+                        </div>
+                        <div className="text">
+                            <a href="https://www.linkedin.com/in/christopher-decker-74263b160/">LinkedIn profile</a>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
--- src/helpers/icons.js
+++ src/helpers/icons.js
@@ -1,8 +1,8 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faTrash, faSignOutAlt, faEdit, faSpinner, faPlusCircle } from "@fortawesome/free-solid-svg-icons";
+import { faTrash, faSignOutAlt, faEdit, faSpinner, faPlusCircle, faMobile, faPaw, faLink, faFile } from "@fortawesome/free-solid-svg-icons";
 
 const Icons = () => {
-return library.add(faTrash, faSignOutAlt, faEdit, faSpinner, faPlusCircle);
+return library.add(faTrash, faSignOutAlt, faEdit, faSpinner, faPlusCircle, faMobile, faPaw, faLink, faFile );
 };
 
 export default Icons;
