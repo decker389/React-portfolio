9b7a89dd2730459c998642eb2ed7606c3d602f23
Mr. Decker <decker389@aol.com>
2021-02-03 14:33:44 -0700

    added styles for login form and some icons for it as well.

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default class Login extends Component {
     constructor(props) {
@@ -58,26 +59,30 @@ export default class Login extends Component {
                 <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
                 <div>{this.state.errorText}</div>
 
-                <form onSubmit={this.handleSumbit}>
-                    <input 
-                    type="email"
-                    name="email"
-                    placeholder="Your Email"
-                    value={this.state.email}
-                    onChange={this.handleChange}
-                    />
-
-                    <input 
-                    type="password"
-                    name="password"
-                    placeholder="Your password"
-                    value={this.state.password}
-                    onChange={this.handleChange}
-                    />
-
-                    <div>
-                        <button type="submit">Login</button>
+                <form onSubmit={this.handleSumbit} className="auth-form-wrapper">
+                    <div className="form-group">
+                        <FontAwesomeIcon icon="file" />
+                        <input 
+                            type="email"
+                            name="email"
+                            placeholder="Your Email"
+                            value={this.state.email}
+                            onChange={this.handleChange}
+                        />
                     </div>
+
+                    <div className="form-group">
+                        <FontAwesomeIcon icon="lock" />
+                        <input 
+                        type="password"
+                            name="password"
+                            placeholder="Your password"
+                            value={this.state.password}
+                            onChange={this.handleChange}
+                        />
+                    </div>
+
+                    <button type="submit">Login</button>
                 </form>
             </div>
         )
--- src/helpers/icons.js
+++ src/helpers/icons.js
@@ -1,8 +1,8 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faTrash, faSignOutAlt, faEdit, faSpinner, faPlusCircle, faMobile, faPaw, faLink, faFile } from "@fortawesome/free-solid-svg-icons";
+import { faTrash, faSignOutAlt, faEdit, faSpinner, faPlusCircle, faMobile, faPaw, faLink, faFile, faLock } from "@fortawesome/free-solid-svg-icons";
 
 const Icons = () => {
-return library.add(faTrash, faSignOutAlt, faEdit, faSpinner, faPlusCircle, faMobile, faPaw, faLink, faFile );
+return library.add(faTrash, faSignOutAlt, faEdit, faSpinner, faPlusCircle, faMobile, faPaw, faLink, faFile, faLock );
 };
 
 export default Icons;
