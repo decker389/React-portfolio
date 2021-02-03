9858719aa94ee7dfb5dac8ac142abb2ac404297e
Mr. Decker <decker389@aol.com>
2021-02-03 13:03:08 -0700

    changed some styles on the portfolio-details page.

--- src/components/portfolio/portfolio-detail.js
+++ src/components/portfolio/portfolio-detail.js
@@ -35,31 +35,39 @@ export default class PortfolioDetail extends Component {
             thumb_image_url,
             url
         } = this.state.portfolioItem;
+
+        const bannerStyles = {
+            backgroundImage: "url(" + banner_image_url + ")",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center center"
+        }
+
         return (
             <div>
                 <div className="portfolio-detail-wrapper">
+                    <div className="portfolio-banner-img" style={bannerStyles}>
+                        <img src={logo_url} />
+                    </div>
+
                     <div className="portfolio-detail-items">
-                    <h1>Name: </h1> <p>{name}</p>
+                    <h1>Name: </h1> {name}
                     </div>
                 
                     <div className="portfolio-detail-items">
-                    <h1>Category: </h1> <p>{category}</p>
+                    <h1>Category: </h1> {category}
                     </div>
                
                     <div className="portfolio-detail-items">
-                    <h1>Url: </h1> <p>{url}</p>
+                    <h1>Url: </h1> <a href={url} className="site-link" target="_blank">visit {name}</a>
                     </div>
               
                     <div className="portfolio-detail-items">
-                    <h1>Description: </h1> <p>{description}</p>
+                    <h1>Description: </h1> {description}
                     </div>
 
                     <div className="portfolio-detail-items">
                     <h1>Thumb Image: </h1> <img src={thumb_image_url} />
-                    </div>
-
-                    <div className="portfolio-detail-items">
-                    <h1>Banner Image: </h1> <img src={banner_image_url} />
                     </div>
                 
                     <div className="portfolio-detail-items">
