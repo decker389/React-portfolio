7c936b76856f87552de9b39188cdca796b3d8f6d
christopher decker <decker389@aol.com>
2021-03-06 01:30:55 -0700

    updated about-me page information with my story

--- src/components/about/about-info.js
+++ src/components/about/about-info.js
@@ -4,7 +4,26 @@ import React from"react";
 const AboutInfo = () => {
     return (
         <div>
-            <p font-weight="100">random things about me!</p>
+            <p font-weight="150">I graduated High School the spring of 2010
+                <br />I immediately started College at the local community college near me
+                <br />I left Fall semester of 2017 with an Assoiates Degree in General Education: Humanities
+                <br />From  there I was able to get a intership position through my dad at his place of work where he works for an I.T.  
+                <br />department mainly as a AS400 developer. That lasted around three months before I decided I wanted to 
+                go back home.
+                <br /> from there I looked for work, hoping that my expirence working with my dad would be enough to find work 
+                at home. It was not, and I found a job not programming.
+                <br />Because of this I began to again consider moving away from my hometown to find work, but that was not the 
+                only reason. The location I decided on moving to seemed to have what I was looking for in all areas of my life.
+                <br /> from professional oppertunities to personal life goals. I visted in 2019 and made the desicion that I would
+                <br /> move in early 2020 to attend Bottega coding in person. Then covid19 happened, my move got delayed and 
+                <br /> in person classes stopped being available. I finally got to move in June of 2020. I now live in Utah, looking
+                <br /> for all those oppertunities to become reality. 
+                <br /> The current chapter comes to a close and a new chapter starts soon! I hope it will be a great one!
+            </p>
+            <p>The programming languages I have experience with are:
+                <br /> Java(both apps and applets), PHP, C++, Javascript, python, and the React Framework.
+            </p>
+            
         </div>
     )
 }
